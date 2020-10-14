c795e3cc4dfec33ada2dc5ad11e1db5d017b4baf
unknown <priscilaayeleng@gmail.com>
2020-10-14 20:29:02 -0300

    Se cambia proyecto.

--- SchoolTestApi/Startup.cs
+++ SchoolTestApi/Startup.cs
@@ -1,14 +1,20 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using Microsoft.EntityFrameworkCore;
 using Models;
 
-namespace SchoolApi
+namespace SchoolTestApi
 {
     public class Startup
     {
@@ -22,16 +28,14 @@ namespace SchoolApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<SchoolContext>(opt =>
+        services.AddDbContext<SchoolContext>(opt =>
                opt.UseInMemoryDatabase("SchoolDatabase"));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SchoolApi", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SchoolTestApi", Version = "v1" });
             });
-
-            //services.AddTransient
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,8 +45,7 @@ namespace SchoolApi
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SchoolApi v1"));
-                /*Para ingresar a Swagger https://localhost:5001/swagger/index.html*/
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SchoolTestApi v1"));
             }
 
             app.UseHttpsRedirection();
