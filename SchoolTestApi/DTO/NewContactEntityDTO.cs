b68893df427fc50247bd536bb1c5c873a9851fa4
unknown <priscilaayeleng@gmail.com>
2020-10-28 21:02:31 -0300

    Se corrigen errores en el modelo

--- SchoolTestApi/DTO/NewContactEntityDTO.cs
+++ SchoolTestApi/DTO/NewContactEntityDTO.cs
@@ -3,7 +3,7 @@ namespace Models{
         public string Name{get;set;}
         public string Lastname{get;set;}
         public string Email{get;set;}
-        public int PhoneNumber{get;set;}
+        public string PhoneNumber{get;set;}
         public string Content{get;set;}
     }
 }
--- SchoolTestApi/Models/ContactEntity.cs
+++ SchoolTestApi/Models/ContactEntity.cs
@@ -4,7 +4,7 @@ namespace Models{
         public string Name{get;set;}
         public string Lastname{get;set;}
         public string Email{get;set;}
-        public int PhoneNumber{get;set;}
+        public string PhoneNumber{get;set;}
         public string Content{get;set;}
     }
 }
